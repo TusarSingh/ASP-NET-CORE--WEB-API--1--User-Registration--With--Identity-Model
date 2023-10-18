60cdb1b176f03644c03441c542246d9024646312
Tusar Singh <rts0941@gmail.com>
2023-10-18 20:01:53 +0530

    Write Connection String code in Radhey.API/Program.cs

--- Radhey__WebAPI/Radhey.API/Program.cs
+++ Radhey__WebAPI/Radhey.API/Program.cs
@@ -1,8 +1,48 @@
+using Radhey.DAL.DatabaseContext;
+
+//using Microsoft.EntityFrameworkCore.SqlServer;                  //          UseSqlServer
+using Microsoft.EntityFrameworkCore;                              //          UseSqlServer
+using Radhey.DAL.IdentityTables;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
+
+
+
+
+
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<RadheyDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+builder.Services.AddIdentityCore<TblApplicationUser>().AddEntityFrameworkStores<RadheyDbContext>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
