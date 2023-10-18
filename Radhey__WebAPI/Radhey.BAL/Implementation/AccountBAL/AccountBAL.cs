ad4f978e323119cbd54c14fc1b599fc2ca4665e0
Tusar Singh <rts0941@gmail.com>
2023-10-18 16:53:01 +0530

    Create a Project Asp.Net Core Web Api 8.0

---
+++ Radhey__WebAPI/Radhey.BAL/Implementation/AccountBAL/AccountBAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhey.BAL.Implementation.AccountBAL
+{
+    public class AccountBAL
+    {
+    }
+}
---
+++ Radhey__WebAPI/Radhey.BAL/Interface/IAccountBAL/IAccountBAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhey.BAL.Interface.IAccountBAL
+{
+    public interface IAccountBAL
+    {
+    }
+}
