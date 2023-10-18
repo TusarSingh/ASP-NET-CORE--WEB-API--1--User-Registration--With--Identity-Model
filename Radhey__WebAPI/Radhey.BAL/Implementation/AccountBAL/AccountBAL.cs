c45b6db9afefb1d7feb15d5835ee15a4cf59dd79
Tusar Singh <rts0941@gmail.com>
2023-10-19 02:08:52 +0530

    Create a Logic for ' USER REGISTRATION ' in Radhey.BAL.Implementation.AccountBAL.AccountBAL.cs

--- Radhey__WebAPI/Radhey.BAL/Implementation/AccountBAL/AccountBAL.cs
+++ Radhey__WebAPI/Radhey.BAL/Implementation/AccountBAL/AccountBAL.cs
@@ -4,9 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Radhey.BAL.Interface.IAccountBAL;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+using Radhey.Repository;
+using Radhey.Repository.Interface;
+using Radhey.Repository.Interface.IAccountRepo;
+
+
+
 namespace Radhey.BAL.Implementation.AccountBAL
 {
-    public class AccountBAL
+    public class AccountBAL : IAccountBAL
     {
+
+        public readonly IAccountRepo _accountRepo;
+
+
+
+
+        public AccountBAL(IAccountRepo accountRepo)
+        {
+            this._accountRepo = accountRepo; 
+        }
+
+
+
+        public async Task<ResponseComModel> UserRegistration(UserRegistrationReqModel userRegistrationReq)
+        {
+            var response = new ResponseComModel();
+
+            response = await _accountRepo.UserRegistration(userRegistrationReq).ConfigureAwait(false);
+            
+            return response;
+
+        }
+
+
+
+
+
+
     }
 }
