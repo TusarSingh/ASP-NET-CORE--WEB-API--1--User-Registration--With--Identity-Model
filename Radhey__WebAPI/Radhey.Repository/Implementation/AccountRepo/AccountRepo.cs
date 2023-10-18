4d5e0730ebcfae2f9497de66b83107495d3e678d
Tusar Singh <rts0941@gmail.com>
2023-10-19 01:38:58 +0530

    Create a Logic for ' UserRegistration ' in Radhey.Repository.Implementation.AccountRepo.AccountRepo.cs

--- Radhey__WebAPI/Radhey.Repository/Implementation/AccountRepo/AccountRepo.cs
+++ Radhey__WebAPI/Radhey.Repository/Implementation/AccountRepo/AccountRepo.cs
@@ -4,9 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Microsoft.AspNetCore.Identity;
+using Radhey.DAL.IdentityTables;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+using Radhey.Repository.Interface.IAccountRepo;
+
+
+
 namespace Radhey.Repository.Implementation.AccountRepo
 {
-    public class AccountRepo
+    public class AccountRepo : IAccountRepo
     {
+
+        private readonly UserManager<TblApplicationUser> _userManager;
+        
+
+
+
+
+
+
+
+
+        public AccountRepo(UserManager<TblApplicationUser> userManager)
+        {
+                this._userManager = userManager;    
+        }
+
+
+
+
+        public async Task<ResponseComModel> UserRegistration(UserRegistrationReqModel userRegistrationReq)
+        {
+            ResponseComModel response = new ResponseComModel();
+
+            TblApplicationUser User = new TblApplicationUser()
+            {
+                FirstName = userRegistrationReq.FirstName,
+                LastName = userRegistrationReq.LastName,
+                Email = userRegistrationReq.Email,
+                PasswordHash = userRegistrationReq.Password,
+                PhoneNumber = userRegistrationReq.Phone
+            };
+
+            var saveResult = await _userManager.CreateAsync(User).ConfigureAwait(false);
+
+            if (saveResult.Succeeded)
+            {
+                response.StatusCode = 200;
+                response.IsSuccess = true;
+                response.StatusMessage = "Registration Successfully";
+            }
+            else
+            {
+                response.StatusCode = 400;
+                response.IsSuccess = true;
+                response.StatusMessage = "Registration Failed";
+            }
+
+            return response;
+            
+        }
     }
+
+
+
+
+
+
+
+
+
+
 }
