175c1892675c74c00c3efca1e99d67e85b6ccc56
Tusar Singh <rts0941@gmail.com>
2023-10-18 18:46:03 +0530

    Write a DbContext Code  in Radhey.DAL/DatabaseContext/RadheyDbContext.cs

--- Radhey__WebAPI/Radhey.DAL/DatabaseContext/RadheyDbContext.cs
+++ Radhey__WebAPI/Radhey.DAL/DatabaseContext/RadheyDbContext.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Radhey.DAL.IdentityTables;
+using Microsoft.EntityFrameworkCore;      //   : IdentityDbContext < TblApplicationUser >
+
 namespace Radhey.DAL.DatabaseContext
 {
-    public class RadheyDbContext 
+    public class RadheyDbContext : IdentityDbContext<TblApplicationUser>
     {
+        
+        public RadheyDbContext(DbContextOptions<RadheyDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+        public DbSet<TblApplicationUser> AspNetUser {  get; set; }
+
+
     }
 }
