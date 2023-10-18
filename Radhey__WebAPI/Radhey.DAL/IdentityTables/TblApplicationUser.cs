25a50edec84be354908aa1aaf8f332e47ee099a4
Tusar Singh <rts0941@gmail.com>
2023-10-18 18:12:57 +0530

    Create a Properties FirstName in Radhey.DAL/IdentityTables/TblApplicationUser.cs

--- Radhey__WebAPI/Radhey.DAL/IdentityTables/TblApplicationUser.cs
+++ Radhey__WebAPI/Radhey.DAL/IdentityTables/TblApplicationUser.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Microsoft.AspNetCore.Identity;                //       : IdentityUser
+
 namespace Radhey.DAL.IdentityTables
 {
-    public class TblApplicationUser 
+    public class TblApplicationUser : IdentityUser
     {
 
-        
+        public string FirstName { get; set; }
 
 
     }
