1c7069161b96d893f5cfca0aa7171d7a6fe95f5d
Tusar Singh <rts0941@gmail.com>
2023-10-19 01:48:04 +0530

    Add a Method ' USER REGISTRATION ' in Radhey.BAL.Interface.IAccountBAL.IAccountBAL.cs

--- Radhey__WebAPI/Radhey.BAL/Interface/IAccountBAL/IAccountBAL.cs
+++ Radhey__WebAPI/Radhey.BAL/Interface/IAccountBAL/IAccountBAL.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+
+
 namespace Radhey.BAL.Interface.IAccountBAL
 {
     public interface IAccountBAL
     {
+
+        public Task<ResponseComModel> UserRegistration(UserRegistrationReqModel userRegistrationReq);
+
+
     }
 }
